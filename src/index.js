7fe54286f4ff85d9f1840063c441583b7031a2f6
FahriZamzami <fahrizamzami78242@gmail.com>
2025-07-01 22:06:42 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -194,6 +194,6 @@ app.get('/logout', (req, res) => {
 });
 
 // ===== START SERVER =====
-app.listen(443, () => {
-    console.log('🚀 Server berjalan di http://localhost:3001');
+app.listen(4000, () => {
+    console.log('🚀 Server berjalan di http://localhost:4000');
 });
