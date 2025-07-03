325e99a0406949e3673c3663ce2b0c8b002b8a5c
Fahri Zamzami <fahrizamzami78242@gmail.com>
2025-07-04 01:32:16 +0700

    commit again

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ app.use('/', mainRouter);
 app.use('/', adminRouter); 
 
 // ===== START SERVER =====
-const PORT = 4000;
+const PORT = 3002;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
 });
